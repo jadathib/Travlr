6f1a5d6b05cb930fb4c6e5ed958f02d2bb7b5bd8
jadalini <67033826+jadalini@users.noreply.github.com>
2022-07-31 23:05:17 -0500

    Create object schema with Mongoose, populate MongoDB

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,12 +1,12 @@
-const fs = require('fs'); 
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8')); 
+/*GET travel view */
+let fs = require('fs'); 
+let trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8')); 
 
 
 const travel = (req, res) => {
-  pageTitle = process.env.npm_package_description + ' - Travel';
-  res.render('travel', {title: pageTitle, trips});
+   res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
 module.exports = { 
   travel
-}
+};
