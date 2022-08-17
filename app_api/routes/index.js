1d35a36925eaf15552ee3f713d0647de9e761fdf
jadalini <67033826+jadalini@users.noreply.github.com>
2022-08-16 22:20:53 -0500

    changes committed

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -5,10 +5,12 @@ const tripsController = require('../controllers/trips');
 
 router 
     .route('/trips') //take a request for trips
-    .get(tripsController.tripsList); //pass it to the controller
+    .get(tripsController.tripsList) //pass it to the controller
+    .post(tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')//setting the route for the tripCode parameter
-    .get(tripsController.tripsFindCode);// pass it to the controller
+    .get(tripsController.tripsFindCode)// pass it to the controller
+    .put(tripsController.tripsUpdateTrip);
     
 module.exports = router;
