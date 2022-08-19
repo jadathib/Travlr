7e493b3788d3f50168524b1a30dae86164e8a210
jadalini <67033826+jadalini@users.noreply.github.com>
2022-08-19 13:14:46 -0500

    Added Security for backend

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,16 +1,30 @@
 const express = require('express'); 
 const router = express.Router(); 
+const jwt = require('express-jwt');
+const auth = jwt({
+    secret: process.env.JWT_SECRET,
+    userProperty: 'payload'
+});
 
+const authController = require('../controllers/authentication');
 const tripsController = require('../controllers/trips');
 
 router 
+    .route('/login')
+    .post(authController.login);
+
+router 
+    .route('/register')
+    .post(authController.register); 
+
+router
     .route('/trips') //take a request for trips
     .get(tripsController.tripsList) //pass it to the controller
-    .post(tripsController.tripsAddTrip);
+    .post(auth, tripsController.tripsAddTrip);
 
 router
     .route('/trips/:tripCode')//setting the route for the tripCode parameter
     .get(tripsController.tripsFindCode)// pass it to the controller
-    .put(tripsController.tripsUpdateTrip);
+    .put(auth, tripsController.tripsUpdateTrip);
     
 module.exports = router;
