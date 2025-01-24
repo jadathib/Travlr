1e677b4f4f8f3eecffd343fa3e77a691bae97309
Jada <jada.r.thibodeaux@gmail.com>
2025-01-23 23:32:20 -0600

    updated the correct database string

--- .seedgooserc.js
+++ .seedgooserc.js
@@ -2,5 +2,5 @@ module.exports = {
   modelBaseDirectory: 'app_server/models', // model directory name
   models: ['*.js', '!db.js'], // model matcher
   data: 'data', // data directory name
-  db: 'mongodb://localhost:27017/travlr' // db connection url
+  db: 'mongodb+srv://JadaThibodeaux:Bitchpleaze1!@cluster0.z80ru.mongodb.net/' // db connection url
 };
