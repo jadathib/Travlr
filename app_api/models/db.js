a4c5a017576fc5090d2879dff7d20ecedae1bd98
Jada <jada.r.thibodeaux@gmail.com>
2025-01-23 23:33:30 -0600

    updated dbs update

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,46 +1,26 @@
 const mongoose = require('mongoose');
-let dbURI = 'mongodb+srv://JadaThibodeaux:Bitchpleaze1!@cluster0.z80ru.mongodb.net/';
-if (process.env.NODE_ENV === 'production') {
-  dbURI = process.env.MONGODB_URI;
-}
-mongoose.connect(dbURI);
 
+// Use your connection string here
+const dbURI = 'mongodb+srv://JadaThibodeaux:Bitchpleaze1!@cluster0.z80ru.mongodb.net/';
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Mongoose connected successfully'))
+    .catch(err => console.log('Mongoose connection error:', err));
+
+
+// Log database connection events
 mongoose.connection.on('connected', () => {
   console.log(`Mongoose connected to ${dbURI}`);
 });
-mongoose.connection.on('error', err => {
-  console.log('Mongoose connection error:', err);
+
+mongoose.connection.on('error', (err) => {
+  console.log(`Mongoose connection error: ${err}`);
 });
+
 mongoose.connection.on('disconnected', () => {
   console.log('Mongoose disconnected');
 });
 
-const gracefulShutdown = (msg, callback) => {
-  mongoose.connection.close( () => {
-    console.log(`Mongoose disconnected through ${msg}`);
-    callback();
-  });
-};
-
-// For nodemon restarts                                 
-process.once('SIGUSR2', () => {
-  gracefulShutdown('nodemon restart', () => {
-    process.kill(process.pid, 'SIGUSR2');
-  });
-});
-// For app termination
-process.on('SIGINT', () => {
-  gracefulShutdown('app termination', () => {
-    process.exit(0);
-  });
-});
-// For Heroku app termination
-process.on('SIGTERM', () => {
-  gracefulShutdown('Heroku app shutdown', () => {
-    process.exit(0);
-  });
-});
-
-
-require('./travlr');
-require('./user');
+// Import schemas
+require('./user'); // Ensure the `user` schema is registered
+require('./trips'); // Add this line to register the `trips` schema
