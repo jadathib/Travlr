4c18ac2f4b9c3095ef3cfa36d25844e2da6966d0
Jada <jada.r.thibodeaux@gmail.com>
2025-01-23 23:32:34 -0600

    app changes with cors

--- app.js
+++ app.js
@@ -1,32 +1,21 @@
 require('dotenv').config();
-
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const hbs = require('hbs');
-require('./app_api/models/db'); //bring in the database code from mongoose
-const passport = require('passport'); 
-
-require('./app_api/models/db'); 
+require('./app_api/models/db');
+const passport = require('passport');
 require('./app_api/config/passport');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
-const apiRouter = require('./app_api/routes/index');//add route for API
+const apiRouter = require('./app_api/routes/index');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'app_server', 'views'));
-
-// register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
-
-app.set('view engine', 'hbs');
-
+// Middleware setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -34,42 +23,38 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
 
-// allow CORS 
+// Allow CORS - Updated to handle all routes that start with '/api'
 app.use('/api', (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); 
-res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'); 
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); 
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');  // Allow frontend domain
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allow all necessary methods
   next();
 });
 
+// Route setup
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
-app.use('/api', apiRouter); //tells the application to use the new route
+app.use('/api', apiRouter);
 
-//catch unathorized error and create 401 
-app.use(function(err, req, res, next) { 
+// Error handling
+app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
-    res 
-      .status(401) 
-      .json({ "message": err.name + ":" + err.message });
+    res.status(401).json({ message: `${err.name}: ${err.message}` });
+  } else {
+    next(err);
   }
 });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, _next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+// Error handler
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ error: err.message });
 });
 
 module.exports = app;
